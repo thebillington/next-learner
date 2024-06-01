a651aec77acacc1da1660f7fdf0a9807d8caba0d
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-06-01 15:12:25 +0100

    Follow through tutorial to step 6 (lol should've made a repo sooner)

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const PrimaryFont = Inter({ subsets: ['latin']});
+export const SecondaryFont = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
